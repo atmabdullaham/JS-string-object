2e02872ff9c6fb33956a6fcddbba26d6864f92bf
atmabdullaham <atmabdullaham@gmail.com>
2024-11-09 16:41:46 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,27 @@
+const school = "Raj UK"
+console.log(school.toLowerCase());
+
+
+// console.log(school);
+
+const subject = "chemistry";
+const book = "chemistry";
+
+if (subject === book) {
+ console.log("i am reading book")
+} else {
+ console.log("huadai")
+}
+
+console.log(subject.toUpperCase())
+
+// toUpperCase
+// toLowerCase
+
+const drink = " water";
+const liquid = "water";
+if (drink.trim === liquid.trim) {
+ console.log("pani er opor name life")
+} else {
+ console.log("somodder a pani ase")
+}
---
+++ string.js
@@ -0,0 +1,24 @@
+const country = 'Bangladehs';
+const division = "Newkhali";
+
+const district = `B-Baria`;
+// const thana = new String(`Demra`);
+
+
+const numbers = [51, 23, 54, 64];
+console.log(numbers.length);
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+
+const capital = "Dhaka";
+console.log(capital.length);
+console.log(capital[0]);
+console.log(capital[1]);
+console.log(capital[2]);
+console.log(capital[3]);
+console.log(capital);
+capital[1] = "m";
+console.log(capital);
+
+// String is immutable .
